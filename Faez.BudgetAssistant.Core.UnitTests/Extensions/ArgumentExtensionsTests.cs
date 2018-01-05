5cff5e7e5b82d53198d00c0991b8c00adbef1f3d
Faez <faez.ridzal@gmail.com>
2018-01-05 23:40:54 +0100

    First solution and core project commit

---
+++ Faez.BudgetAssistant.Core.UnitTests/Extensions/ArgumentExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Faez.BudgetAssistant.Core.UnitTests.Extensions
+{
+    using System;
+    using Core.Extensions;
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class ArgumentExtensionsTests
+    {
+        [Test]
+        public void ShouldThrowExceptions()
+        {
+            Action nullObject = () => ArgumentExtensions.EnsureArgumentNotNull<object>(null, "argumentName");
+
+            nullObject.ShouldThrow<ArgumentNullException>();
+
+            Action emptyString = () => ArgumentExtensions.EnsureArgumentNotNullOrWhitespace(string.Empty, "argumentName");
+            Action whitespaceString = () => ArgumentExtensions.EnsureArgumentNotNullOrWhitespace(" ", "argumentName");
+
+            emptyString.ShouldThrow<ArgumentNullException>();
+            whitespaceString.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ Faez.BudgetAssistant.Core/Extensions/ArgumentExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Faez.BudgetAssistant.Core.Extensions
+{
+    using System;
+
+    public static class ArgumentExtensions
+    {
+        public static T EnsureArgumentNotNull<T>(this T value, string name)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+
+            return value;
+        }
+
+        public static string EnsureArgumentNotNullOrWhitespace(this string value, string name)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException(name, "Cannot be null or whitespace");
+            }
+
+            return value;
+        }
+    }
+}
