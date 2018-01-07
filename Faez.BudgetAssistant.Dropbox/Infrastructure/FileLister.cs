d22791667bf857fc8c581b7d6716e45d16f10a17
Faez <faez.ridzal@gmail.com>
2018-01-07 13:57:23 +0100

    Initial dropbox connector

---
+++ Faez.BudgetAssistant.Dropbox/Infrastructure/FileLister.cs
@@ -0,0 +1,38 @@
+﻿namespace Faez.BudgetAssistant.Dropbox.Infrastructure
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using global::Dropbox.Api;
+
+    public sealed class FileLister : IFileLister
+    {
+        private const string ApplicationRoot = "/Apps/Faez.BudgetAssistant";
+
+        private readonly DropboxClient _client;
+
+        public FileLister() : this("IPmI4s7uf24AAAAAAAAC5SYxqMCtJ5pvXLlVR8PfnyQmcvbT9T4F0Q_t2yQH60nh")
+        {
+        }
+
+        private FileLister(string accessToken)
+        {
+            _client = new DropboxClient(accessToken);
+        }
+
+        public async Task<IReadOnlyCollection<string>> GetFiles()
+        {
+            var folders = await _client.Files.ListFolderAsync(ApplicationRoot);
+            
+            return folders.Entries
+                .Where(e => e.IsFile)
+                .Select(e => e.PathDisplay)
+                .ToList();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ Faez.BudgetAssistant.Dropbox/Infrastructure/IFileLister.cs
@@ -0,0 +1,11 @@
+﻿namespace Faez.BudgetAssistant.Dropbox.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IFileLister : IDisposable
+    {
+        Task<IReadOnlyCollection<string>> GetFiles();
+    }
+}
