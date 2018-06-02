237aa597778420706ba72a632e893c1a4dd664e8
Faez <faez.ridzal@gmail.com>
2018-06-02 21:44:14 +0200

    Added Maybank forex reader, unit tested too

---
+++ Faez.BudgetAssistant.Forex.UnitTests/DataLayer/MaybankForexReaderTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Faez.BudgetAssistant.Forex.UnitTests.DataLayer
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using BudgetAssistant.UnitTests.Helpers;
+    using FluentAssertions;
+    using Forex.DataLayer;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public sealed class MaybankForexReaderTests
+    {
+        private const string FilePath = "Files\\Maybank.html";
+
+        [Test]
+        public void CorrectParsing()
+        {
+            var file = Path.Combine(PathHelper.GetAssemblyPath(), FilePath);
+            var payload = File.ReadAllText(file);
+            var rate = new MaybankForexReader().ReadAll(payload).Single();
+
+            rate.Date.Should().Be(DateTime.Today);
+            rate.FromCurrency.Should().Be("MYR");
+            rate.ToCurrency.Should().Be("EUR");
+            rate.Rate.Should().Be(4.714M);
+        }
+    }
+}
--- Faez.BudgetAssistant.Forex/DataLayer/MaybankForexReader.cs
+++ Faez.BudgetAssistant.Forex/DataLayer/MaybankForexReader.cs
@@ -1,13 +1,37 @@
 ﻿namespace Faez.BudgetAssistant.Forex.DataLayer
 {
+    using System;
     using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using HtmlAgilityPack;
     using Models;
 
     public sealed class MaybankForexReader : IForexReader
     {
         public IEnumerable<ForexRate> ReadAll(string payload)
         {
-            return null;
+            var document = new HtmlDocument();
+
+            document.LoadHtml(payload);
+
+            var qualifiedNodes = document.DocumentNode.SelectNodes("//tr");
+            var sellNode = qualifiedNodes.FirstOrDefault(n => n.ChildNodes.Any(c => c.InnerText == "EUR"));
+
+            if (sellNode == null)
+            {
+                yield break;
+            }
+
+            var sellRate = decimal.Parse(sellNode.ChildNodes.Skip(11).Take(1).Single().InnerText, CultureInfo.InvariantCulture);
+
+            yield return new ForexRate
+            {
+                Date = DateTime.Today,
+                FromCurrency = "MYR",
+                ToCurrency = "EUR",
+                Rate = sellRate
+            };
         }
     }
 }
